7a7e0c46ec484fbba8e18bff18e6aa7ec7e1f3d5
tohru-ymgc <jimmy33hiza@yahoo.co.jp>
2021-09-18 22:31:56 +0900

    ::を付けてからメッセージを送るとfizzbuzzが起動するようにした

--- index.js
+++ index.js
@@ -6,30 +6,36 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    const ch_name = "独り言";
-    client.channels.forEach(channel =>{
-        if(channel.name === ch_name){
-            channel.send("オンラインになりました。/n;;+文字入力でfizzbuzzが起動します。");
-        };
-    });
+    //const ch_name = "一般";
+    //client.channels.forEach(channel =>{
+    //    if(channel.name === ch_name){
+    //        channel.send("オンラインになりました。/n::+文字入力でfizzbuzzが起動します。");
+    //    };
+    //});
 });
 
 client.on('message', message => {
+    const fizzbuzz = ()=>{
+        let messagebox = message.content.replace("::","");
+        let check = Number(messagebox);
+        if (Number.isInteger(check) === false) { 
+            message.reply("整数を入力してね。");
+        }else if (check%15 === 0) {
+                message.reply("fizzbuzz");
+            }else if(check%5 === 0) {
+                    message.reply("buzz");
+                }else if(check%3 === 0) {
+                    message.reply("fizz");
+                    }else{
+                        message.reply(check)
+                    }    
+    };
     if(message.author.bot) return;
     if(message.channel.name !== "一般") return;
-    let messagebox = message.content.replace(";;","");
-    let check = Number(message.content);
-    if (Number.isInteger(check) === false) { 
-        message.reply("整数を入力してね。");
-    }else if (check%15 === 0) {
-            message.reply("fizzbuzz");
-        }else if(check%5 === 0) {
-                message.reply("buzz");
-            }else if(check%3 === 0) {
-                message.reply("fizz");
-                }else{
-                    message.reply(check)
-                }
+    if(message.content.indexOf("::") != -1){
+        fizzbuzz();
+    }
+
 });
 
 client.login(process.env.DISCORD_TOKEN)
